92f8fda0bcda2cec7b1c1b914938b45d23d4e3a8
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2024-06-24 16:57:25 +0530

    navbar reponsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="fixed right-0 top-0 w-full px-4 flex gap-8 items-center bg-blue-900">
-      <div className="w-[20%]">
+      <div className="w-[20%] max-md:w-full flex flex-col items-center justify-center">
         <img
           src="https://logolook.net/wp-content/uploads/2021/11/HDFC-Bank-Logo.svg"
           width={150}
@@ -9,7 +9,7 @@ export default function Navbar() {
           alt="HDFC Bank Logo"
         />
       </div>
-      <div className=" w-[80%] flex  justify-evenly items-center text-white text-xl">
+      <div className=" max-md:hidden w-[80%] flex  justify-evenly items-center text-white text-xl">
         <p className="cursor-pointer">Home</p>
         <p className="cursor-pointer">Pay</p>
         <p className="cursor-pointer">Save</p>
